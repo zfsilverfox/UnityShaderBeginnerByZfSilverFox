268f961b83f4e5d98af1e7c9b1b2127f371a0cc0
zfsilverfox <47052123+zfsilverfox@users.noreply.github.com>
2020-05-11 13:12:36 +0900

    Update

---
+++ Assets/Script/DissoveTriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissoveTriggerScript : MonoBehaviour
+{
+    public SkinnedMeshRenderer skinMeshRenderer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) 
+        {
+            skinMeshRenderer.material.SetFloat("Vector1_3C12C0AC", Time.time);
+        }
+    }
+
+
+
+
+
+}
